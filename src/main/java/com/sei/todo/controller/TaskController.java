4617227c2dab7c3baf304273bb0555baa6c0bf48
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-05 20:00:49 -0500

    get single task

--- src/main/java/com/sei/todo/controller/TaskController.java
+++ src/main/java/com/sei/todo/controller/TaskController.java
@@ -1,11 +1,16 @@
 package com.sei.todo.controller;
 
+import com.sei.todo.model.Task;
 import com.sei.todo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/api")
@@ -19,13 +24,15 @@ public class TaskController {
     }
 
     @GetMapping(path="/tasks")
-    public String getTasks(){
-        return "getTasks() ===> Called";
+    public List<Task> getTasks(){
+        System.out.println("getTasks() ===> Called");
+        return taskRepository.findAll();
     }
 
     @GetMapping(path="/tasks/{taskId}")
-    public String getTask(){
-        return "get this task ";
+    public Task getTask(@PathVariable Long taskId){
+        System.out.println("get this task " + taskId);
+        return taskRepository.findById(taskId).get();
     }
 
 }
