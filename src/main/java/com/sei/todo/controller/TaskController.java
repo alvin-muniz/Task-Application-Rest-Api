60130c2e369d90675449809b02b25390d2854e60
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-05 21:03:30 -0500

    completed delete method and update method

--- src/main/java/com/sei/todo/controller/TaskController.java
+++ src/main/java/com/sei/todo/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.sei.todo.controller;
 
 import com.sei.todo.exceptions.InformationExistsException;
+import com.sei.todo.exceptions.InformationNotFoundException;
 import com.sei.todo.model.Task;
 import com.sei.todo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +31,16 @@ public class TaskController {
     @GetMapping(path="/tasks/{taskId}")
     public Task getTask(@PathVariable Long taskId){
         System.out.println("get this task " + taskId);
-        return taskRepository.findById(taskId).get();
+        Optional foundTask = taskRepository.findById(taskId);
+        if(foundTask.isPresent())
+        {
+            return taskRepository.findById(taskId).get();
+        }
+        else
+            {
+                throw new InformationNotFoundException("THis id does not exist");
+            }
+
     }
 
     @PostMapping(path="/tasks")
@@ -41,6 +51,46 @@ public class TaskController {
             throw new InformationExistsException("This task already exists!");
         else
             return this.taskRepository.save(taskObject);
+    }
+    @PutMapping(path="/tasks/{taskId}")
+    public Task updateTask(@PathVariable Long taskId, @RequestBody Task taskObject)
+    {
+        System.out.println("Update Tasks Called");
+
+        Optional foundTask = this.taskRepository.findById(taskId);
+        if(foundTask.isPresent()){
+            Task task = (Task)foundTask.get();
+            System.out.println("This task is present");
+            if(task.equals(taskObject)){
+                throw new InformationExistsException("These are the same objects");
+            }else{
+                boolean completedSame = task.isCompleted()==taskObject.isCompleted() ? true:false;
+                if(!completedSame) task.setCompleted(taskObject.isCompleted());
+                boolean sameName = task.getName().equals(taskObject.getName())?true : false;
+                if(!sameName) task.setName(taskObject.getName());
+                boolean sameDescription = task.getDescription().equals(taskObject.getDescription())? true:false;
+                if(!sameDescription) task.setDescription(taskObject.getDescription());
+                return this.taskRepository.save(task);
+            }
+        }else{
+            throw new InformationNotFoundException("Task doesn't exist to update");
+        }
+
+    }
+
+    @DeleteMapping(path="/tasks/{taskId}")
+    public void deleteTask(@PathVariable Long taskId){
+        System.out.println("delete this task " + taskId);
+        Optional foundTask = taskRepository.findById(taskId);
+        if(foundTask.isPresent())
+        {
+            taskRepository.deleteById(taskId);
+            System.out.println("THis task is deleted " + taskId);
+        }
+        else
+        {
+            throw new InformationNotFoundException("THis id does not exist");
+        }
 
     }
 
