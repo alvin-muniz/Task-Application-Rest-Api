5c58e9444c7bd50de74c3ff632b84a3dc977446e
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-05 20:28:30 -0500

    working create task repository

--- src/main/java/com/sei/todo/controller/TaskController.java
+++ src/main/java/com/sei/todo/controller/TaskController.java
@@ -1,13 +1,11 @@
 package com.sei.todo.controller;
 
+import com.sei.todo.exceptions.InformationExistsException;
 import com.sei.todo.model.Task;
 import com.sei.todo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +31,17 @@ public class TaskController {
     public Task getTask(@PathVariable Long taskId){
         System.out.println("get this task " + taskId);
         return taskRepository.findById(taskId).get();
+    }
+
+    @PostMapping(path="/tasks")
+    public Task createTask(@RequestBody Task taskObject){
+
+        Optional<Task> foundTask = Optional.ofNullable(this.taskRepository.findByName(taskObject.getName()));
+        if(foundTask.isPresent())
+            throw new InformationExistsException("This task already exists!");
+        else
+            return this.taskRepository.save(taskObject);
+
     }
 
 }
---
+++ src/main/java/com/sei/todo/exceptions/InformationExistsException.java
@@ -0,0 +1,14 @@
+package com.sei.todo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InformationExistsException extends RuntimeException {
+
+        public InformationExistsException(String message)
+        {
+            super(message);
+        }
+
+}
