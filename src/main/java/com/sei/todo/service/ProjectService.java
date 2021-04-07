3492ad7e986e75778a01a387d827a7e4ce3ebc16
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-07 07:02:22 -0500

    added getProjectTask and Tasks to project service

--- src/main/java/com/sei/todo/service/ProjectService.java
+++ src/main/java/com/sei/todo/service/ProjectService.java
@@ -3,13 +3,13 @@ package com.sei.todo.service;
 import com.sei.todo.exceptions.InformationExistsException;
 import com.sei.todo.exceptions.InformationNotFoundException;
 import com.sei.todo.model.Project;
+import com.sei.todo.model.Task;
 import com.sei.todo.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -72,6 +72,48 @@ public class ProjectService {
         } else {
             throw new InformationNotFoundException("Cannot delete a category that does not exists");
         }
+    }
+
+//    @GetMapping(path = "/projects/{projectId}/tasks")
+    public List<Task> getProjectTasks(Long projectId){
+        System.out.println("Project Service retrieving tasks in project");
+        return this.getProject(projectId).getTaskList();
+    }
+
+//    @GetMapping(path = "/projects/{projectId}/tasks/{taskId}")
+    public Task getProjectTask(Long projectId,
+                               Long taskId){
+        Project project = getProject(projectId);
+        Optional task =
+                Optional.ofNullable(project.getTaskList().stream().filter(task1 -> task1.getId().equals(taskId)).findFirst());
+        if(task.isPresent())
+        {
+            return (Task)task.get();
+        }
+        else
+            {
+                throw new InformationNotFoundException("This task is not in " +
+                        "this project");
+            }
+    }
+
+  //  @PostMapping(path = "/projects/{projectId}/tasks")
+    public void createProjectTask(Long projectId){
+
+    }
+
+  //  @PutMapping(path = "/projects/{projectId}/tasks/{taskId}")
+    public void updateProjectTask(Long projectId,
+                                  Long taskId,
+                                  Task taskObject){
+
+    }
+
+ //   @DeleteMapping(path = "/projects/{projectId}/tasks/{taskId}")
+    public void deleteProjectTask(Long projectId,
+                                  Long taskId)
+    {
+
     }
 
 }
